d5b69eb00155a496e8251f0aaf322dcae5f39b08
Twice22 <victor.busa@gmail.com>
2018-07-02 21:57:15 +0200

    Add DP python sol to 091

--- 091-DecodeWays/Python/main.py
+++ 091-DecodeWays/Python/main.py
@@ -1,14 +1,34 @@
 class Solution:
+    def numDecodings2(self, s):
+        # transform numDecodings2 to DP
+        if not s:
+            return 0
+        
+        # dp[1] = 1 means if one digit -> one solution
+        dp = [1, 1]
+        for i, digit in enumerate(s):
+            num = 0
+            if digit != "0":
+                num += dp[-1]
+            if i >= 1 and 10 <= int(s[i-1:i+1]) <= 26:
+                num += dp[-2]
+            if not num:
+                return 0
+            dp.append(num)
+        
+        return dp[-1]
+    
     def numDecodings(self, s):
         """
         :type s: str
         :rtype: int
         """
         # idea on an example:
-        # numDecodign(15245) -> numDecoding(1524) + 1 (5)
-        # + numDecoding(152) + (1 if 45 < 26 else 0)
+        # numDecodign(15245) -> numDecoding(1524) + numDecoding(152)
+        # (1 if 10 <= 45 <= 26 else 0)
         return self.numDecodingRec(s, {})
-        
+    
+    # Note: if we factorize if/elif/else... TLE so keep it this way
     def numDecodingRec(self, s, registered):
         if s in registered:
             return registered[s]
